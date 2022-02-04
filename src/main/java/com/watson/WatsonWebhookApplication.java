31f2d477d0c6c6fc914d47fb9c52727f8bac131b
baraths99 <barath.sundaram1999@gmail.com>
2022-02-04 10:50:59 +0530

    intial commit

---
+++ src/main/java/com/watson/WatsonWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.watson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatsonWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatsonWebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watson/WatsonWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatsonWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
